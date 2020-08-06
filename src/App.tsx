d7bf6c0464b9b08afba590ac15299039317c5e17
medvecky <alexey.medvecky@gmail.com>
2020-08-06 23:13:14 +0300

    Changing BrowserRouter to HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import PTMContainer from "./components/PTMContainer/PTMContainer";
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 
 const App: React.FC = () => {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <div className='App'>
                 <PTMContainer/>
             </div>
-        </BrowserRouter>
+        </HashRouter>
     );
 };
 
