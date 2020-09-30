2b9b09b97a9d4c8f8b392209677343f6177314e9
medvecky <alexey.medvecky@gmail.com>
2020-09-30 19:10:58 +0000

    adding new tests in ProjectForm.test.tsx

--- src/unit-tests/ProjectForm.test.tsx
+++ src/unit-tests/ProjectForm.test.tsx
@@ -2,18 +2,24 @@ import {configure, mount, shallow, ShallowWrapper} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import React from "react";
 import ProjectForm from "../components/ProjectForm";
-import {Card} from 'react-bootstrap';
-import {configureStore} from "@reduxjs/toolkit";
+// @ts-ignore
+import configureStore from 'redux-mock-store';
 import {Provider} from "react-redux";
-import store from '../store/store';
+import {HashRouter} from "react-router-dom";
+
 configure({adapter: new Adapter()});
 
 describe('<ProjectFrorm />', () => {
-    let wrapper: ShallowWrapper<any, Readonly<{}>, React.Component<{}, {}, any>>;
+    const initialState = {output: 100}
+    const mockStore = configureStore();
+    // @ts-ignore
+    let wrapper;
+    let store;
     beforeEach(() => {
-
-        wrapper = shallow(
+        store = mockStore(initialState);
+        wrapper = mount(
             <Provider store={store}>
+                <HashRouter>
                 <ProjectForm
                     project={{
                         id: 'xxx',
@@ -22,15 +28,44 @@ describe('<ProjectFrorm />', () => {
                     }}
                     onDeleteProject={() => {
                     }}/>
-            </Provider>).dive().dive();
+                </HashRouter>
+            </Provider>
+        );
     });
 
     it('should render <Card.Title>', () => {
         // @ts-ignore
-        console.log(component.debug());
+        expect(wrapper.find('CardTitle')).toHaveLength(1);
+    });
 
+    it('should render <Card.Title> with correct text', () => {
         // @ts-ignore
-        expect(component.find(Card)).toHaveLength(1);
+        expect(wrapper.find('CardTitle').text()).toEqual('test_title');
+    });
+
+    it('should render <Card.Text>', () => {
+        // @ts-ignore
+        expect(wrapper.find('CardText')).toHaveLength(1);
+    });
+
+    it('should render <Card.Text> with correct text', () => {
+        // @ts-ignore
+        expect(wrapper.find('CardText').text()).toEqual('test_desc');
+    });
+
+    it('should render two <Button>', () => {
+        // @ts-ignore
+        expect(wrapper.find('Button')).toHaveLength(2);
+    });
+
+    it('should render Edit <Button>', () => {
+        // @ts-ignore
+        expect(wrapper.find('Button').at(0).text()).toEqual('Edit');
+    });
+
+    it('should render Delete <Button>', () => {
+        // @ts-ignore
+        expect(wrapper.find('Button').at(1).text()).toEqual('Delete');
     });
 
 });
